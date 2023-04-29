ab1657ead29824068654085993345e18c8b9469b
sultanmirzapydev <sultanmirza136id@gmail.com>
2023-04-29 17:53:30 +0530

    pushed app.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component }  from 'react';
+import Homepage from './files/pages/Homepage';
+
+
+function App() {
+  return (
+    
+      <Homepage/>
+    
+    
+  );
+}
+
+export default App;
